98fb2e2cc690681a387cd9f211b5c31e80db3d2a
loa <loanapumayali14@gmail.com>
2023-09-11 21:53:34 -0500

    SP listar usuarios

--- src/main/java/main/Test.java
+++ src/main/java/main/Test.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import model.TbUsuario;
 
@@ -15,7 +16,11 @@ public class Test {
 		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("JPA_clase01");
 		EntityManager em = fabrica.createEntityManager();
 		
-		List<TbUsuario> lstUsuarios = em.createQuery("Select a From TbUsuario a", TbUsuario.class).getResultList();
+		Query query = em.createNativeQuery("{call FINDALL_USU()}", TbUsuario.class);
+		
+		List<TbUsuario> lstUsuarios = query.getResultList();
+		
+		//List<TbUsuario> lstUsuarios = em.createQuery("Select a From TbUsuario a", TbUsuario.class).getResultList();
 		System.out.println("==============================================");
 		
 		/*Mostrar cantidad de usuarios*/
